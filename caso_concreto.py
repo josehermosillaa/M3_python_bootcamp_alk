51351b0f7ebef074ba2493fe9ac259f8f1eb0076
jose.hermosillaa <jose.hermosillaa@usach.cl>
2026-01-22 21:57:22 -0300

    otros datos

---
+++ caso_concreto.py
@@ -0,0 +1,38 @@
+usuarios = [
+    {
+        "nombre": "Ana",
+        "edad": 30,
+        "email": "ana@mail.com",
+        "ocupacion": "Avenger",
+    },
+    {
+        "nombre": "Diana",
+        "edad": 30,
+        "email": "ana@mail.com",
+        "ocupacion": "Avenger",
+    },
+    {
+        "nombre": "Diana",
+        "edad": 30,
+        "email": "ana@mail.com",
+        "ocupacion": "Avenger",
+    },{
+        "nombre": "Diana",
+        "edad": 30,
+        "email": "ana@mail.com",
+        "ocupacion": "Avenger",
+    }]
+
+
+#les piden usar condicionales si es con usuario puede usar las categorias 
+#categoria que tendra valpores segun las edades
+
+# if edad >= 0:
+#     if 0 <= edad <= 12:
+#         return "niño(a)" 
+#     elif 13 <= edad <= 17:
+#         return "adolescente"
+#     elif 18 <= edad <= 64:
+#         return "adulto"
+#     elif  edad >= 65:
+#         return "mayor"
